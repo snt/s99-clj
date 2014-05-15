a04487d845c2e556803d4eeb1f31c3c90fe7da5d
snt <shintok@gmail.com>
2014-05-15 16:16:30 +0900

    omit loop

--- src/s99clj/core.clj
+++ src/s99clj/core.clj
@@ -3,30 +3,28 @@
 ;;; http://aperiodic.net/phil/scala/s-99/
 
 ; P01
-; clojure does not have TCO, so use loop-recur instead.
+; Clojure does not have TCO, so use `recur` instead.
+; If you do not use `loop`, recursion point is `fn`.
+
 (defn my-last [xs]
-  (loop [xs xs]
-    (let [h (first xs)
-          ts (rest xs)]
-      (if (= '() ts)
-        h
-        (recur ts)))))
+  (let [h (first xs)
+        ts (rest xs)]
+    (if (= '() ts)
+      h
+      (recur ts))))
 
 ; P02
 (defn my-last-but-one [xs]
-  (loop [xs xs]
-    (let [h1 (first xs)
-          h2 (first (rest xs))
-          ts (rest (rest xs))]
-      (if (= '() ts)
-        (if-not (nil? h2) h1 nil)
-        (recur (rest xs))))))
+  (let [h1 (first xs)
+        h2 (first (rest xs))
+        ts (rest (rest xs))]
+    (if (= '() ts)
+      (if-not (nil? h2) h1 nil)
+      (recur (rest xs)))))
 
 ;P03
 (defn my-nth [n xs]
-  (loop [n n
-         xs xs]
-    (if (= n 0)
-      (first xs)
-      (recur (dec n) (rest xs)))))
+  (if (= n 0)
+    (first xs)
+    (recur (dec n) (rest xs))))
 
