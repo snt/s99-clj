46869c45b2de2b37ca19baaf029ce23e86c9a784
snt <shintok@gmail.com>
2014-05-20 00:12:49 +0900

    project.clj updated to use Clojure 1.6.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject s99clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :description "http://aperiodic.net/phil/scala/s-99/ in Clojure"
+  :url "https://github.com/snt/s99-clj"
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/mit-license.php"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
